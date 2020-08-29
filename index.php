0d19dc77d71e431393db8c74b8cc950249fca113
Yehor Bublyk <yehor.bk@gmail.com>
2020-08-29 14:12:16 +0300

    unnecessary code removed

--- index.php
+++ index.php
@@ -12,55 +12,7 @@
 </head>
 <body>
     <?php
-        $pldb = new PLDB();
-        /*$pldb->loadDatabase("db/pldb-test.db.json");
-        $pldb->loadDatabase("db/pldb-real.db.json");
-        print_r($pldb->getDatabasesNames());
-        $database = $pldb->selectDatabase('pldb-real');
-        $table = $database->selectTable('users');
-        print_r($table->select(null));*/
-
-        //print_r($pldb->loadDatabase("db/pldb-test.db.json"));
-        //$pldb->dropDatabase("pldb-test");
-        $database = $pldb->createDatabase('pldb-test');
-        $sheme = array(
-            "name" => "text"
-        );
-        $table = $database->createTable('users', $sheme);
-
-        class User {
-            public $name;
-        }
-        $user = new User();
-        $user->name = "John";
-
-        $user1 = array(
-            "name" => "Marston"
-        );
-
-        $table->insert($user);
-        $table->insert($user1);
-        
-        $condition = array(
-            "id" => 1,
-            "name" => "Doe"
-        );
-
-        //$entries = $table->select($condition);
-        //print_r($entries);
-
-        //$table->delete($condition);
-        $table->update($condition);
-
-        $entries = $table->select(null);
-        //print_r($entries);
-        //$database->dropTable("users");
-        print_r($database->getTablesNames());
-        $pldb->saveDatabase($database);
-
-        //$table->select($condition);
-        //$database = new Database("pldb-test");*/
-        // $result = $database->sendQuery("CREATE DATABASE `users`");
+        // Getting Started //
     ?>
 </body>
 </html>
